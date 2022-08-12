5d6cdd86c99bd0d7cb53a548756ec8244a8a6007
SONGJINSE2 <open8137@gmail.com>
2022-08-12 18:33:59 +0900

    study

--- 7weeks/20220810_03/index.js
+++ 7weeks/20220810_03/index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
 
-const router = require("./routes");
+const router = require("./routes/user");
 app.use('/user', router);
 
 app.listen(port, ()=>{
