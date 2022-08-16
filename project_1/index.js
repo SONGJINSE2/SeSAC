7906a0351a481428d9404bd0d4e8c1482029d586
SONGJINSE2 <open8137@gmail.com>
2022-08-17 01:08:38 +0900

    study

--- project_1/index.js
+++ project_1/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
 const bodyParser = require("body-parser");
 
+
 app.set("view engine", "ejs");
+app.use( "/static", express.static( "static" ) );
 app.use(express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
 
-const router = require("./routes/user");
-app.use('/user', router);
+app.get('/test', (req,res) => {
+    res.render("index");
+});
+
+app.get('/test2', (req,res) => {
+    res.render("stats");
+});
 
 app.listen(port, ()=>{
     console.log( "Server Port : ", port );
